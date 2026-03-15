8069e02fcd021ead348456b0df7a73d0142f971f
AngelBandres2x <angab06@gmail.com>
2026-03-15 19:56:51 -0400

    new: added identification and validity of ELF binaries

---
+++ README.md
@@ -0,0 +1,24 @@
+# Binalyzer
+A CLI tool that serves as a binary analyzer for both ELF and PE formats
+
+## What does it do?
+Five main functionalities
+
+| Functionality | Description | Status |
+| --- | --- | --- |
+| 1. | Identification of the type of binary (ELF/PE) and validity | Done |
+| 2. | Display and parse the main header (ELF header / DOS + COFF headers) | TODO |
+| 3. | List sections (ELF) / segments (PE) with their attributes (name, size, offset, permissions) | TODO |
+| 4. | Extract and show strings | TODO |
+| 5. | Show imported and exported functions | TODO |
+
+## Requirements to run
+- Python installed in your system
+- A binary file
+
+## How to run
+- Download the python file
+- run
+
+## Why?
+This is a personal project, to learn about binary files and their structure depending on the Operating System
---
+++ binalyzer.py
@@ -0,0 +1,43 @@
+# binary analyzer
+import os
+import struct
+import sys
+
+def help_message(): # for proper use of running the script
+    print("Usage: python bin_analyzer.py <path to binary> <arguments>")
+    print("Available arguments:")
+    print("-h: Help (this same page)")
+    print("-a: Analyze")
+
+# validate command line arguments
+if (len(sys.argv) != 3 or "-h" in sys.argv):
+    help_message()
+elif(len(sys.argv) == 3 and sys.argv[2] == "-a"):
+    
+    # open the file in binary mode
+    with open(f"{sys.argv[1]}", "rb") as file:
+        first_64_bytes = struct.unpack("64c", file.read(64)) # read first 64 bytes of the bin
+        # print("first 64 bytes:", first_64_bytes)
+        
+        # 1. validate type of binary
+        if (first_64_bytes[:4] == (b'\x7F', b'E', b'L', b'F')): # if the first 4 bytes are \x7fELF
+            print("It's an ELF binary!")
+
+            # check for the class of the binary
+            ei_class = struct.unpack("b", first_64_bytes[4])[0]
+
+            if (ei_class == 1):
+                print("Its architecture is 32-bit")
+            elif(ei_class == 2):
+                print("Its architecture is 64-bit")
+            else:
+                print("Error: invalid class. Must be 1 or 2. Returned value:", ei_class)
+
+            #2. Parse the main header: TODO
+
+
+        file.close()
+
+
+else:
+    help_message()
