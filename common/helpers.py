bcfb57f119473b1f58bbc162789b978d43060339
AngelBandres2x <angab06@gmail.com>
2026-03-29 12:31:44 -0400

    new: added helper function: recursively print a dictionary

---
+++ common/helpers.py
@@ -0,0 +1,17 @@
+# common/helpers.py: helper functions that will be used all around the project
+
+def rprint_dict(x=dict, level=0):
+    """
+    Recursively prints a nested dictionary in indented hierarchical order
+    
+    Args:
+        x(dict): Given dictionary. Must be provided
+        level(int): Level of hierarchy of the provided dictionary. Level 0 indicates the root dictionary, Levels >1 indicate a dictionary inside the root/parent dictionary
+    """
+
+    for key, value in x.items():
+        if str(type(value)) == "<class \'dict\'>": # if the value is another dictionary
+            print(f"{'\t' * level}{key} :")
+            rprint_dict(value, level+1)
+        else:
+            print(f"{'\t' * level}{key} : {value}")
