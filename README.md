6270aaa928c7d83d1914a737655363f20a7261b2
AngelBandres2x <angab06@gmail.com>
2026-05-02 17:47:44 -0400

    Phase 3: Completed for PE files

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Five main functionalities
 | --- | --- | --- |
 | 1. | Identification of the type of binary (ELF/PE) and validity | Done |
 | 2. | Display and parse the main header (ELF header / DOS + COFF headers) | Done |
-| 3. | List sections (ELF) / segments (PE) with their attributes (name, size, offset, permissions) | ELF |
+| 3. | List sections (ELF) / segments (PE) with their attributes (name, size, offset, permissions) | Done |
 | 4. | Extract and show strings | TODO |
 | 5. | Show imported and exported functions | TODO |
 
--- analyzers/pe_analyzer.py
+++ analyzers/pe_analyzer.py
@@ -22,33 +22,25 @@ def pe_machine_type(machine=int) -> str:
 
     return result
 
-def pe_fh_flags(characteristics=int) -> dict:
+def get_flags(characteristics=int, av_flags=dict) -> dict:
     """
-    Indicates information about the Characteristics of the PE binary file header
+    Indicates information about the characteristics of a certain aspect of the PE file
     
     Args:
-        characteristics(int): Taken from the Characteristics field of the PE File Header. Contains flags to be extracted in hex 
+        characteristics(int): Taken from the Characteristics field from a PE Header. Contains flags to be extracted in hex
+        ref_flags(dict): Dictionary of available flags of a certain aspect of the binary, taken from common/constants.py
 
     Returns:
         flags(dict): A dictionary that contains attributes of the object or image file. Follows the format: {flag_description: flag_hex_value, ...}
     """
 
-    flags = dict()
+    flags = dict() # resulting dictionary to be returned
 
-    aux_c = characteristics
-    i = 0
-
-    for key, value in c.IMAGE_FILE_CHARACTERISTICS.items():
-        if ((aux_c % 16) * (16 ** i)) == value: # if the current digit is a valid flag
-            flags[key] = hex(value)
-            
-            # move to the next most significant digit
-            aux_c = aux_c // 16 
-            i += 1
-        if (aux_c == 0): break
+    for key in av_flags.keys():
+        if characteristics & key == key:
+            flags[hex(key)] = av_flags[key]
 
     return flags
-    
 
 def parse_dos_header(file=None):
     """DOS Header Parser"""
@@ -89,7 +81,7 @@ def parse_pe_header(header=str, file=None) -> dict:
         file_header["PointerToSymbolTable"] = header_info[3]
         file_header["NumberOfSymbols"] = header_info[4]
         file_header["SizeOfOptionalHeader (bytes)"] = header_info[5]
-        file_header["Characteristics"] = pe_fh_flags(header_info[6])
+        file_header["Characteristics"] = get_flags(header_info[6], c.IMAGE_FILE_CHARACTERISTICS)
 
         pe_header_info["File Header"] = file_header
 
@@ -117,57 +109,59 @@ def parse_pe_header(header=str, file=None) -> dict:
 
         return pe_header_info
 
-def list_sections(header=str, pe_header=dict(), file=None):
-    """Lists the sections"""
+def list_sections(header=dict(), file=None):
+    """
+    Parses through each section of the binary and returns a list that contains information of each one
+
+    Args:
+        header(dict): The PE header, required to be able to be able to get the offset for reading the Sections. Must be provided and non-empty
+        file(__file__): File object, required to read each entry from the Section Header Table. Must be provided and non-empty
+    
+    Returns:
+        section_list(list): A list of dictionaries, where each dictionary represents the struct that holds all of the information related to the section
+
+    Raises:
+        ValueError: When either the header or the file object is empty or isn't provided.
+    """
     if file is None:
         raise ValueError("error: non-empty file object must be provided")
     elif header is None:
         raise ValueError("error: non-empty header must be provided")
-    elif pe_header is None:
-        raise ValueError("Error: non-empty PE Header dictionary must be provided")
 
     def get_name(name=str) -> str:
-        """Get the decoded name of a string"""
+        """Get the decoded name off of a string of bytes"""
         # conditional to avoid errors while I figure out how to properly extract the name
         if b'/' in name:
-            return name
+            return name # TODO : extract the name properly from the string table
         else:
-            name = name.replace(b'\x00', b'')
-            return name.decode('utf-8', errors="replace")
+            name = name.replace(b'\x00', b'') # Delete all null bytes
+            return name.decode('utf-8', errors="replace") # Error handling if necessary
             
-    def get_flags(characteristics=int()) -> dict:
-        raise NotImplementedError
-
-    num_sections = pe_header["File Header"]["NumberOfSections"]
-    print("There are", num_sections, "sections")
+    num_sections = header["File Header"]["NumberOfSections"]
 
     # section table offset: COFF Offset value, 4 for signature, 20 for coff file header + SizeOfOptionalHeader
-    st_off = pe_header["COFF Offset"] + 4 + 20 + pe_header["File Header"]["SizeOfOptionalHeader (bytes)"] # section table offset
-    print("Current file position:", file.tell())
+    st_off = header["COFF Offset"] + 4 + 20 + header["File Header"]["SizeOfOptionalHeader (bytes)"] # section table offset
     file.seek(st_off, 0)
-    print("File position after seeking to st_off:", file.tell())
     
     section_list = list()
     for i in range(num_sections):
         section = dict()
         section["Name"] = get_name(struct.unpack("<8s", file.read(8))[0])
-        section["VirtualSize"] = struct.unpack("<I", file.read(4))[0]
-        section["VirtualAddress"] = struct.unpack("<I", file.read(4))[0]
-        section["SizeOfRawData"] = struct.unpack("<I", file.read(4))[0]
-        section["PointerToRawData"] = struct.unpack("<I", file.read(4))[0]
-        section["PointerToRelocations"] = struct.unpack("<I", file.read(4))[0]
-        section["PointerToLinenumbers"] = struct.unpack("<I", file.read(4))[0]
-        section["NumberOfRelocations"] = struct.unpack("<H", file.read(2))[0]
-        section["NumberOfLinenumbers"] = struct.unpack("<H", file.read(2))[0]
-        section["Characteristics"] = hex(struct.unpack("<I", file.read(4))[0])
+        section["VirtualSize"] = hex(struct.unpack("<I", file.read(4))[0])
+        section["VirtualAddress"] = hex(struct.unpack("<I", file.read(4))[0])
+        section["SizeOfRawData"] = hex(struct.unpack("<I", file.read(4))[0])
+        section["PointerToRawData"] = hex(struct.unpack("<I", file.read(4))[0])
+        section["PointerToRelocations"] = hex(struct.unpack("<I", file.read(4))[0])
+        section["PointerToLinenumbers"] = hex(struct.unpack("<I", file.read(4))[0])
+        section["NumberOfRelocations"] = hex(struct.unpack("<H", file.read(2))[0])
+        section["NumberOfLinenumbers"] = hex(struct.unpack("<H", file.read(2))[0])
+        section["Characteristics"] = get_flags(struct.unpack("<I", file.read(4))[0], c.IMAGE_SECTION_CHARACTERISTICS)
         section_list.append(section)
 
     return section_list
-    
-
 
 def analyze(header, file=None) -> dict:
     info = dict()
     info["PE Header"] = parse_pe_header(header, file)
-    info["Sections"] = list_sections(header, info["PE Header"], file)
+    info["Sections"] = list_sections(info["PE Header"], file)
     return info
